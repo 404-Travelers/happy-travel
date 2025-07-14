04202e47cb6e379f5636c7009e85f0d28b3a606d
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-07-14 15:12:11 +0200

    feat(UserServiceTest): add mocks

--- src/test/java/com/_travelers/happy_travel/users/UserServiceTest.java
+++ src/test/java/com/_travelers/happy_travel/users/UserServiceTest.java
@@ -1,4 +1,28 @@
 package com._travelers.happy_travel.users;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {}
+    
+    @AfterEach
+    void afterTest(){
+        verifyNoMoreInteractions(userRepository);
+    }
+
 }
