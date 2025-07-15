c5b5332808a0b56a8d358755d907608aeb08448e
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-07-15 14:58:02 +0200

    feat(UserService): add UserDetailsService implentation and loadUserByUsername method

--- src/main/java/com/_travelers/happy_travel/users/UserService.java
+++ src/main/java/com/_travelers/happy_travel/users/UserService.java
@@ -1,10 +1,18 @@
 package com._travelers.happy_travel.users;
 
-public class UserService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
     User user;
 
     public void test(){
         user.getRole();
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
