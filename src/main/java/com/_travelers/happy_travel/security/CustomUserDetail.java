4a99772a3c7e555e629dd3abbd17964a10496b7a
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-07-15 15:26:41 +0200

    fix(CustomUserDetail):fix imports

--- src/main/java/com/_travelers/happy_travel/security/CustomUserDetail.java
+++ src/main/java/com/_travelers/happy_travel/security/CustomUserDetail.java
@@ -1,8 +1,8 @@
 package com._travelers.happy_travel.security;
 
+import com._travelers.happy_travel.users.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -17,7 +17,7 @@ public class CustomUserDetail implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(user.getRole()));
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
     }
 
     @Override
