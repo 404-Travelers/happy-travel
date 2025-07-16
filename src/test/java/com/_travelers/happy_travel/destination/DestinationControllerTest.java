f148ed431c705952a8f535a8d43d2226fcf9315e
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-07-16 10:16:00 +0200

    test(DestinationControllerTest): add getAll method

--- src/test/java/com/_travelers/happy_travel/destination/DestinationControllerTest.java
+++ src/test/java/com/_travelers/happy_travel/destination/DestinationControllerTest.java
@@ -4,13 +4,23 @@ import com._travelers.happy_travel.destinations.DestinationService;
 import com._travelers.happy_travel.destinations.dto.DestinationRequest;
 import com._travelers.happy_travel.destinations.dto.DestinationResponse;
 import com._travelers.happy_travel.destinations.dto.DestinationResponseShort;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,13 +38,22 @@ public class DestinationControllerTest{
 
     private DestinationResponseShort destinationResponseShort;
     private String jsonDestinationRequest;
-    private DestinationResponse destinationResponse1;
-    private DestinationResponse destinationResponse2;
+    private DestinationResponse destinationResponse;
     private DestinationRequest destinationRequest;
     private Long id;
 
-    @BeforeEach(){
+    @BeforeEach
+    void setUp() {
     }
 
+    @Test
+    void getAllDestinations_whenDestinationsExist_returnsListOfDestinationsResponseShort() throws JsonProcessingException {
+        List<DestinationResponseShort> serviceResult = List.of(new DestinationResponseShort());
+        String expectedJson = objectMapper.writeValueAsString(List.of(new DestinationResponseShort()));
+        given(destinationService.getAllDestinations()).willReturn(serviceResult);
 
+        mockMvc.perform(get("/destinations").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson));
+    }
 }
