553f619ccda20264d3649330b6466365d174bb97
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-07-16 14:53:36 +0200

    feat(user/dtos): add UserLoginRequest, UserRegisterRequest, UserResponse, UserMapper

--- src/main/java/com/_travelers/happy_travel/users/dto/UserMapper.java
+++ src/main/java/com/_travelers/happy_travel/users/dto/UserMapper.java
@@ -1,4 +1,24 @@
 package com._travelers.happy_travel.users.dto;
 
+import com._travelers.happy_travel.users.User;
+import com._travelers.happy_travel.users.Role;
+
 public class UserMapper {
+
+    public static User toEntity(UserRegisterRequest dto) {
+        return User.builder()
+                .username(dto.username())
+                .email(dto.email())
+                .password(dto.password())
+                .role(Role.USER)
+                .build();
+    }
+
+    public static UserResponse toDto(User user) {
+        return new UserResponse(
+                user.getUsername(),
+                user.getEmail(),
+                user.getRole().name()
+        );
+    }
 }
