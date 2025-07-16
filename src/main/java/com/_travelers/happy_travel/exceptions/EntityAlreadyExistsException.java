4e3f98e7a24238dd9fa85d09cb97cab371cf6d1e
saba <sabaurrehman11@gmail.com>
2025-07-16 10:41:35 +0200

    feat(exceptions): create EntityNotFoundException and EntityAlreadyExistsException

--- src/main/java/com/_travelers/happy_travel/exceptions/EntityAlreadyExistsException.java
+++ src/main/java/com/_travelers/happy_travel/exceptions/EntityAlreadyExistsException.java
@@ -1,4 +1,7 @@
 package com._travelers.happy_travel.exceptions;
 
-public class EntityAlreadyExistsException {
+public class EntityAlreadyExistsException extends RuntimeException {
+    public EntityAlreadyExistsException(String entityClass, String attributeName, String attributeValue) {
+        super(String.format("%s with %s %s already exists", entityClass, attributeName, attributeValue));
+    }
 }
--- src/main/java/com/_travelers/happy_travel/exceptions/EntityNotFoundException.java
+++ src/main/java/com/_travelers/happy_travel/exceptions/EntityNotFoundException.java
@@ -1,4 +1,7 @@
 package com._travelers.happy_travel.exceptions;
 
-public class EntityNotFoundException {
+public class EntityNotFoundException extends RuntimeException{
+    public EntityNotFoundException(String entityClass, String attributeName, String attributeValue) {
+        super(String.format("%s with %s %s not found", entityClass, attributeName, attributeValue));
+    }
 }
