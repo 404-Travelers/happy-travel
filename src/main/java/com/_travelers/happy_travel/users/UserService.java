d454aa73e2c195615c52dbba2a2ae2807bd4c043
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-07-17 13:54:11 +0200

    feat(user): add UserRepository, UserService, UserController

--- src/main/java/com/_travelers/happy_travel/users/UserService.java
+++ src/main/java/com/_travelers/happy_travel/users/UserService.java
@@ -1,10 +1,57 @@
 package com._travelers.happy_travel.users;
 
-public class UserService {
-    User user;
+import com._travelers.happy_travel.users.dto.UserMapper;
+import com._travelers.happy_travel.users.dto.UserRegisterRequest;
+import com._travelers.happy_travel.users.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-    public void test(){
-        user.getRole();
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserResponse addUser(UserRegisterRequest request) {
+        String encodedPassword = passwordEncoder.encode(request.password());
+
+        var user = UserMapper.toEntity(
+                new UserRegisterRequest(
+                        request.username(),
+                        request.email(),
+                        encodedPassword
+                )
+        );
+
+        userRepository.save(user);
+        return UserMapper.toDto(user);
+
+    }
+    public UserResponse getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User with id " + id + " not found"));
+        return UserMapper.toDto(user);
     }
 
+    public void deleteUser(Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new EntityNotFoundException("User with id " + id + " not found");
+        }
+        userRepository.deleteById(id);
+    }
+
+    public UserResponse updateUser(Long id, UserRegisterRequest request) {
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User with id " + id + " not found"));
+
+        existingUser.setUsername(request.username());
+        existingUser.setEmail(request.email());
+        existingUser.setPassword(passwordEncoder.encode(request.password())); // шифруем пароль
+
+        User updatedUser = userRepository.save(existingUser);
+        return UserMapper.toDto(updatedUser);
+    }
 }
+
--- src/main/java/com/_travelers/happy_travel/users/dto/UserResponse.java
+++ src/main/java/com/_travelers/happy_travel/users/dto/UserResponse.java
@@ -3,9 +3,6 @@ package com._travelers.happy_travel.users.dto;
 import lombok.Builder;
 import lombok.Data;
 
-@Data
-@Builder
-
 public record UserResponse (
     String username,
     String email,
