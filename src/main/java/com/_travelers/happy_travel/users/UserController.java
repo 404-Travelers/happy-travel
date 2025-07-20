bdaee3fac5975cc787d786aca8f919388c4a2829
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-07-20 15:27:31 +0200

    feat(user): add Swagger documentation for user endpoints

--- src/main/java/com/_travelers/happy_travel/users/UserController.java
+++ src/main/java/com/_travelers/happy_travel/users/UserController.java
@@ -2,6 +2,7 @@ package com._travelers.happy_travel.users;
 
 import com._travelers.happy_travel.users.dto.UserRegisterRequest;
 import com._travelers.happy_travel.users.dto.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,24 +18,39 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(
+           summary = "Resister a new user",
+           description = "Creates a new user with default role 'USER'. Requires valid username, email, and password."
+            )
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody @Valid UserRegisterRequest userRegisterRequest) {
         UserResponse createdUser = userService.addUser(userRegisterRequest);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get user by ID",
+            description = "Returns a user by ID. Throws error if not found."
+    )
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
         UserResponse user = userService.getUserById(id);
         return ResponseEntity.ok(user);
     }
-
+    @Operation(
+        summary = "Delete user by ID",
+        description = "Deletes user with given ID. Returns 204 if successful."
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Update user by ID",
+            description = "Updates an existing user’s username, email, or password."
+    )
     @PutMapping("/{id}")
     public ResponseEntity<UserResponse> updateUser(
             @PathVariable Long id,
