787e04ed45add0fce969a15cad5de8430621f6ec
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-07-22 12:47:19 +0200

    feat(DestinationController): add getAllDestinations endpoint

--- src/main/java/com/_travelers/happy_travel/destinations/DestinationController.java
+++ src/main/java/com/_travelers/happy_travel/destinations/DestinationController.java
@@ -1,4 +1,37 @@
 package com._travelers.happy_travel.destinations;
 
+
+import com._travelers.happy_travel.destinations.dto.DestinationResponseShort;
+import com._travelers.happy_travel.users.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/destinations")
 public class DestinationController {
+
+    private final DestinationService destinationService;
+    private final UserService userService;
+
+    public DestinationController(DestinationService destinationService, UserService userService) {
+        this.destinationService = destinationService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DestinationResponseShort>> getAllDestinations() {
+        List<DestinationResponseShort> list = destinationService.getAllDestinations()
+                .stream()
+                .map(dest -> new DestinationResponseShort(
+                        dest.country(),
+                        dest.city(),
+                        dest.imageUrl(),
+                        dest.user()
+                ))
+                .toList();
+        return ResponseEntity.ok(list);
+    }
+
+
 }
