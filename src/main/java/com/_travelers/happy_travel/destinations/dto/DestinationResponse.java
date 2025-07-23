954f725bc989b53c0560618ab805570ace8122e3
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-07-23 15:02:50 +0200

    fix(DestinationMapper, DestinationResponseShort, DestinationResponse): change UserResponse for UserResponseShort

--- src/main/java/com/_travelers/happy_travel/destinations/dto/DestinationResponse.java
+++ src/main/java/com/_travelers/happy_travel/destinations/dto/DestinationResponse.java
@@ -2,12 +2,13 @@ package com._travelers.happy_travel.destinations.dto;
 
 import com._travelers.happy_travel.users.User;
 import com._travelers.happy_travel.users.dto.UserResponse;
+import com._travelers.happy_travel.users.dto.UserResponseShort;
 
 public record DestinationResponse(
         String country,
         String city,
         String description,
         String imageUrl,
-        UserResponse user
+        UserResponseShort user
 ) {
 }
