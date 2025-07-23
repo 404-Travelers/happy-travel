44138bd5aed494afa4bde07ddee24bf03698332c
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-07-23 14:45:55 +0200

    fix(UserRegisterRequest): change validation and messages

--- src/main/java/com/_travelers/happy_travel/users/dto/UserRegisterRequest.java
+++ src/main/java/com/_travelers/happy_travel/users/dto/UserRegisterRequest.java
@@ -1,21 +1,23 @@
 package com._travelers.happy_travel.users.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 
 public record UserRegisterRequest(
     @NotEmpty(message = "Username is required")
-    @Size(min = 5, max = 50, message = "Username must be between 3 and 50 characters")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     String username,
 
     @Email(message = "Email should be valid")
     @NotEmpty(message = "Email is required")
     @Size(min = 5, max = 100, message = "Email must be between 5 and 100 characters")
+    @Email(message = "Email not valid", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     String email,
 
     @NotEmpty(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(message = "Password must contain a minimum of 8 characters, including a number, one uppercase letter, one lowercase letter and one special character", regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=.])(?=\\S+$).{8,}$")
     String password
 ) {
 }
