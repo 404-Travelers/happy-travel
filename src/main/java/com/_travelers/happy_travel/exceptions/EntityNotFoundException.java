a57e54f47310b0eb0db3442d3bb7d52956b191c6
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-07-30 09:30:08 +0200

    fix(exceptions): add quotes to error message in EntityAlreadyExistsEx and EntityNotFoundEx

--- src/main/java/com/_travelers/happy_travel/exceptions/EntityNotFoundException.java
+++ src/main/java/com/_travelers/happy_travel/exceptions/EntityNotFoundException.java
@@ -2,6 +2,6 @@ package com._travelers.happy_travel.exceptions;
 
 public class EntityNotFoundException extends RuntimeException{
     public EntityNotFoundException(String entityClass, String attributeName, String attributeValue) {
-        super(String.format("%s with %s %s not found", entityClass, attributeName, attributeValue));
+        super(String.format("%s with %s \"%s\" not found", entityClass, attributeName, attributeValue));
     }
 }
